b0bec36302abf3bf7e2b41c4bee98f2f409ec8e0
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-10-22 20:07:29 -0700

    improve plotting

--- scripts/read_pkl.py
+++ scripts/read_pkl.py
@@ -1,44 +1,73 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-from constants.unitree_legged_const import LegID
+from collections import defaultdict
+from constants.unitree_legged_const import LegID, JOINT_LIMITS
 
 if __name__ == "__main__":
-    with (open("hips.pkl", "rb")) as openfile:
-        joint_commands = pickle.load(openfile)
-        joint_states = pickle.load(openfile)
+    pkl_and_motors = {
+        "hips.pkl": ["RL_0", "RR_0", "FL_0", "FR_0"],
+        "RL_1.pkl": ["RL_1"],
+        "RR_1.pkl": ["RR_1"],
+        "FL_1.pkl": ["FL_1"],
+        "FR_1.pkl": ["FR_1"],
+        "calves.pkl": ["RL_2", "RR_2", "FL_2", "FR_2"],
+    }
+    motors_to_plot = ["RL_0", "RR_0", "FL_0", "FR_0","RL_1", "RR_1", "FL_1", "FR_1", "RL_2", "RR_2", "FL_2", "FR_2"]
 
-    motors_to_plot = ["RL_0", "RR_0", "FL_0", "FR_0","RL_1", "RR_1", "FL_1", "FR_1","RL_2", "RR_2", "FL_2", "FR_2"]
-    time_offset = joint_commands[0][0]
-    joint_command_times = [command[0] - time_offset for command in joint_commands]
-    joint_command_q = [[command[1][LegID[motor_id]] for motor_id in motors_to_plot] for command in joint_commands]
+    # key is motor, value is tuple of time and command
+    commands_to_plot = defaultdict(lambda: [[], []])
+    states_to_plot = defaultdict(lambda: [[], []])
 
-    joint_state_times = [state[0] - time_offset for state in joint_states]
-    joint_state_q = [[state[1][LegID[motor_id]].q for motor_id in motors_to_plot] for state in joint_states]
+    for pkl_file, motors in pkl_and_motors.items():
+        with (open(pkl_file, "rb")) as openfile:
+            joint_commands = pickle.load(openfile)
+            joint_states = pickle.load(openfile)
+            info = pickle.load(openfile)
 
-    command_time_diffs = np.diff(joint_command_times)
-    print(f"Publisher commands are separated by {np.mean(command_time_diffs)} seconds with variance {np.var(command_time_diffs)}. {1/np.mean(command_time_diffs)} Hz")
+        time_offset = joint_commands[0][0]
+        joint_command_times = [command[0] - time_offset for command in joint_commands]
+        for motor in motors:
+            commands_to_plot[motor][0] = joint_command_times
+        for time_command in joint_commands:
+            for motor in motors:
+                commands_to_plot[motor][1].append(time_command[1][LegID[motor]])
 
-    state_time_diffs = np.diff(joint_state_times)
-    print(f"Subscriber states are separated by {np.mean(state_time_diffs)} seconds with variance {np.var(state_time_diffs)}. {1/np.mean(state_time_diffs)} Hz")
+        time_offset = joint_states[0][0]
+        joint_state_times = [state[0] - time_offset for state in joint_states]
+        for motor in motors:
+            states_to_plot[motor][0] = joint_state_times
+        for time_state in joint_states:
+            for motor in motors:
+                states_to_plot[motor][1].append(time_state[1][LegID[motor]].q)
 
-    # plotting
+    # command_time_diffs = np.diff(joint_command_times)
+    # print(f"Publisher commands are separated by {np.mean(command_time_diffs)} seconds with variance {np.var(command_time_diffs)}. {1/np.mean(command_time_diffs)} Hz")
+
+    # state_time_diffs = np.diff(joint_state_times)
+    # print(f"Subscriber states are separated by {np.mean(state_time_diffs)} seconds with variance {np.var(state_time_diffs)}. {1/np.mean(state_time_diffs)} Hz")
+
     # Plotting
     fig, axs = plt.subplots(3,4, figsize=(16, 10), sharex=True)
     fig.suptitle('Joint Commands and Joint States over Time')
 
     for joint_idx in range(len(motors_to_plot)):
+        motor = motors_to_plot[joint_idx]
         # Extract joint command and state for the current joint
-        joint_cmd = [cmd[joint_idx] for cmd in joint_command_q]
-        joint_state = [state[joint_idx] for state in joint_state_q]
+        joint_cmd = commands_to_plot[motor][1]
+        joint_state = states_to_plot[motor][1]
         
         # Plot joint command
-        axs.flatten()[joint_idx].plot(joint_command_times, joint_cmd, label=f'Command', color='b')
+        axs.flatten()[joint_idx].plot(commands_to_plot[motor][0], joint_cmd, label=f'Command', color='b')
         
         # Plot joint state
-        axs.flatten()[joint_idx].plot(joint_state_times, joint_state, label=f'State', color='r', linestyle='--')
+        axs.flatten()[joint_idx].plot(states_to_plot[motor][0], joint_state, label=f'State', color='r', linestyle='--')
+
+        # plot joint limits
+        axs.flatten()[joint_idx].axhline(JOINT_LIMITS[motor][0], color='black', linestyle="dotted")
+        axs.flatten()[joint_idx].axhline(JOINT_LIMITS[motor][1], color='black', linestyle="dotted")
         
-        axs.flatten()[joint_idx].set_ylabel(f'Joint {motors_to_plot[joint_idx]} angle (rad)')
+        axs.flatten()[joint_idx].set_ylabel(f'Joint {motor} angle (rad)')
         axs.flatten()[joint_idx].legend()
 
     axs.flatten()[-1].set_xlabel('Time')
