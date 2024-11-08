896ee31d3dc43bc8ad288afd3db3204d0edd9dd5
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-07 19:52:12 -0800

    first try at ramp rate

---
+++ examples/ramp_rate.py
@@ -0,0 +1,164 @@
+import time
+import sys
+import math
+import pickle
+import copy
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
+from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowState_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_
+from unitree_sdk2py.utils.crc import CRC
+from unitree_sdk2py.utils.thread import Thread
+import constants.unitree_legged_const as go2
+import utils.client_utils as client_utils
+
+'''
+IMPORTANT: Turn off sport mode beforehand. Must suspend robot with harness.
+'''
+
+PUB_FREQ = 250
+PUB_PERIOD = 1/PUB_FREQ
+
+RAMP_RATES = [2,4,6,8] # rad / s
+MAX_RADIANS = 30
+
+crc = CRC()
+
+joint_state_log = []
+joint_command_log = []
+
+def read_joints(msg: LowState_):
+    # thread-safety https://stackoverflow.com/a/18568017
+    joint_state_log.append((time.time(), msg.motor_state))
+
+def move_to_initial_pose(cmd, pub, motors, target_positions):
+
+    for name in motors:
+        cmd.motor_cmd[go2.LegID[name]].mode = 0x01
+        cmd.motor_cmd[go2.LegID[name]].q = target_positions[name]
+        cmd.motor_cmd[go2.LegID[name]].kp = 1.0 
+        cmd.motor_cmd[go2.LegID[name]].dq = 0.0
+        cmd.motor_cmd[go2.LegID[name]].kd = 1.0
+        cmd.motor_cmd[go2.LegID[name]].tau = 0.0
+
+    while True:
+        cmd.crc = crc.Crc(cmd)
+        if pub.Write(cmd):
+            joint_command_log.append((time.time(), [x.q for x in cmd.motor_cmd]))
+        else:
+            print("Waiting for subscriber.")
+
+        time.sleep(1)
+
+        latest_joint_states = joint_state_log[-1][1]
+        reached_target = True
+        for motor in motors:
+            if not math.isclose(latest_joint_states[go2.LegID[motor]].q, target_positions[motor], abs_tol=0.001):
+                reached_target = False
+        if reached_target:
+            break
+    
+    for name in motors:
+        cmd.motor_cmd[go2.LegID[name]].mode = 0x01
+        cmd.motor_cmd[go2.LegID[name]].q = target_positions[motor]
+        cmd.motor_cmd[go2.LegID[name]].kp = 10.0 
+        cmd.motor_cmd[go2.LegID[name]].dq = 0.0
+        cmd.motor_cmd[go2.LegID[name]].kd = 1.0
+        cmd.motor_cmd[go2.LegID[name]].tau = 0.0
+
+    print("Finished setting motors to initial position.")
+
+if __name__ == '__main__':
+
+    if len(sys.argv)>1:
+        ChannelFactoryInitialize(0, sys.argv[1])
+    else:
+        ChannelFactoryInitialize(0)
+
+    rsc = client_utils.get_robot_state_client()
+    client_utils.set_service(rsc, "sport_mode", False)
+
+    # Create a publisher to publish the data defined in UserData class
+    pub = ChannelPublisher("rt/lowcmd", LowCmd_)
+    pub.Init()
+
+    sub = ChannelSubscriber("rt/lowstate", LowState_)
+    sub.Init(read_joints, 10)
+    
+    cmd = unitree_go_msg_dds__LowCmd_()
+    cmd.head[0]=0xFE
+    cmd.head[1]=0xEF
+    cmd.level_flag = 0xFF
+    cmd.gpio = 0
+    for i in range(20):
+        cmd.motor_cmd[i].mode = 0x01  # (PMSM) mode
+        cmd.motor_cmd[i].q= go2.PosStopF
+        cmd.motor_cmd[i].kp = 0
+        cmd.motor_cmd[i].dq = go2.VelStopF
+        cmd.motor_cmd[i].kd = 0
+        cmd.motor_cmd[i].tau = 0
+
+    target_positions = {}
+    for motor, limits in go2.JOINT_LIMITS.items():
+        target_positions[motor] = (limits[0] + limits[1]) / 2
+
+    hip_motors = ["RL_0", "RR_0", "FL_0", "FR_0"]
+    move_to_initial_pose(cmd, pub, hip_motors, target_positions)
+
+    # motors_to_control = ["FR_2"] #, "RR_2", "FL_2", "FR_2"]
+    motors_to_control = ["FR_1"] #, "RR_1", "FL_1", "FR_1"]
+    # motors_to_control = ["FR_0"] #, "RR_0", "FL_0", "FR_0"]
+
+    # sets first position to q=0
+    move_to_initial_pose(cmd, pub, motors_to_control, target_positions)
+
+    ramp_index = 0
+    period_index = 0
+
+    next_time = time.time()
+
+    while True:
+        # try going to ramp_rate * pub period (rad)
+        for motor in motors_to_control:
+            change_in_q = RAMP_RATES[ramp_index] * PUB_PERIOD * period_index
+            cmd.motor_cmd[go2.LegID[motor]].q = target_positions[motor] + change_in_q
+
+        cmd.crc = crc.Crc(cmd)
+
+        #Publish message
+        if pub.Write(cmd):
+            joint_command_log.append((time.time(), [x.q for x in cmd.motor_cmd]))
+        else:
+            print("Waiting for subscriber.")
+
+        latest_joint_states = joint_state_log[-1][1]
+        reached_target = True
+        for motor in motors_to_control:
+            if latest_joint_states[go2.LegID[motor]].q < MAX_RADIANS + target_positions[motor]:
+                reached_target = False
+        if reached_target:
+            period_index = -1 # since adding by 1 at the end of this loop
+            ramp_index += 1
+            move_to_initial_pose(cmd, pub, motors_to_control, target_positions)
+            if ramp_index >= len(RAMP_RATES):
+                break
+
+        # Update next_time and sleep for the remaining time
+        period_index += 1
+        next_time += PUB_PERIOD
+        time.sleep(max(0, next_time - time.time()))
+
+    pub.Close()
+    sub.Close()
+
+    print("Closed pub and sub.")
+
+    log_file = "ramp_logs.pkl"
+    with open(log_file, 'wb') as f:
+        pickle.dump(joint_command_log, f)
+        pickle.dump(joint_state_log, f)
+        # pickle.dump({"amplitude": amplitude, "offset": offset, "freq": FREQUENCIES},f)
+
+    print(f"Saved to {log_file}")
--- scripts/read_pkl.py
+++ scripts/read_pkl.py
@@ -4,6 +4,76 @@ import numpy as np
 from collections import defaultdict
 from constants.unitree_legged_const import LegID, JOINT_LIMITS
 
+def get_largest_slope(A, f):
+    f = np.array(f)
+    return np.round(2 * np.pi * f * A, 2)
+
+def frequency_stats(times):
+    # Calculate time intervals (periods)
+    periods = np.diff(times)
+    
+    # Calculate frequencies (reciprocal of periods)
+    frequencies = 1 / periods
+    
+    # Mean and standard deviation of frequencies
+    mean_freq = np.mean(frequencies)
+    std_dev_freq = np.std(frequencies)
+    
+    return mean_freq, std_dev_freq
+
+# Function to find robust crossings within a tolerance range
+def find_crossings(y_data, x_data, level):
+    crossings = []
+    for i in range(2, len(y_data) - 2):
+        # Check if the first two points are below the level and the next two points are ascending above the level
+        if (y_data[i-2] >= level and y_data[i-1] >= level and
+            y_data[i] <= level and y_data[i+1] <= y_data[i]):
+            # Register the crossing point as the midpoint between i-1 and i
+            slope = (y_data[i] - y_data[i-1]) / (x_data[i] - x_data[i-1])
+            crossing_x = x_data[i-1] + (level - y_data[i-1]) / slope
+            crossings.append(crossing_x)
+    return crossings
+
+def get_lag(y_sin, x_sin, y_exp, x_exp, offset):
+    y_sin = np.array(y_sin)
+    x_sin = np.array(x_sin)
+    y_exp = np.array(y_exp)
+    x_exp = np.array(x_exp)
+
+    sin_interp = interp1d(x_sin, y_sin, kind='cubic', fill_value="extrapolate")
+    y_sin_interp = sin_interp(x_exp)
+
+    # Detect crossings in both sinusoidal and experimental data
+    sin_crossings = find_crossings(y_sin_interp, x_exp, offset)
+    exp_crossings = find_crossings(y_exp, x_exp, offset)
+
+    if len(exp_crossings) > 0 and exp_crossings[0] < 3:
+        exp_crossings = np.delete(exp_crossings, 0)
+
+    # Calculate lag for each crossing, ensuring the indexes match in length
+    if len(sin_crossings) != len(exp_crossings):
+        print(f"Double check {sin_crossings}, {exp_crossings}")
+    min_crossings = min(len(sin_crossings), len(exp_crossings))
+    lags = []
+    for i in range(min_crossings):
+        sin_x_cross = sin_crossings[i]
+        exp_x_cross = exp_crossings[i]
+        lag = exp_x_cross - sin_x_cross
+        lags.append(lag)
+
+    # Convert lags list to numpy array for easy manipulation
+    lags = np.array(lags)
+    return lags, sin_crossings, exp_crossings
+
+def remove_spikes(y_sin):
+    # Detect and remove spikes in y_sin by replacing them with average of neighbors
+    threshold = 0.5  # Define a threshold for spike detection
+    for i in range(1, len(y_sin) - 1):
+        if abs(y_sin[i] - y_sin[i-1]) > threshold and abs(y_sin[i] - y_sin[i+1]) > threshold:
+            y_sin[i] = (y_sin[i-1] + y_sin[i+1]) / 2  # Replace spike with average of neighbors
+
+    return y_sin
+
 if __name__ == "__main__":
     # pkl_and_motors = {
     #     "hips.pkl": ["RR_0", "FL_0", "FR_0"],
@@ -19,6 +89,8 @@ if __name__ == "__main__":
     # key is motor, value is tuple of time and command
     commands_to_plot = defaultdict(lambda: [[], []])
     states_to_plot = defaultdict(lambda: [[], []])
+    offsets = {}
+    ramp_rates = {}
 
     for motor in motors_to_plot: # pkl_file, motors in pkl_and_motors.items():
         motors = [motor]
@@ -27,6 +99,10 @@ if __name__ == "__main__":
             joint_commands = pickle.load(openfile)
             joint_states = pickle.load(openfile)
             info = pickle.load(openfile)
+            # print(f"{motor}: {get_largest_slope(info['amplitude'][motor], info['freq'])}")
+            ramp_rate = get_largest_slope(info['amplitude'][motor], info['freq'])
+            ramp_rates[motor] = ramp_rate
+            offsets[motor] = info["offset"][motor]
 
         time_offset = joint_commands[0][0]
         joint_command_times = [command[0] - time_offset for command in joint_commands]
@@ -64,7 +140,10 @@ if __name__ == "__main__":
         joint_state = states_to_plot[motor][1]
         joint_state_time = states_to_plot[motor][0]
 
-        print(f"{motor}: Min: {min(joint_state[10:]) * 180 / np.pi} and max: {max(joint_state[10:]) * 180 / np.pi}")
+        # print(f"{motor}: Average publisher frequency: {frequency_stats(joint_cmd_time)}")
+        # print(f"{motor}: Average subscriber frequency: {frequency_stats(joint_state_time)}")
+
+        # print(f"{motor}: Min: {min(joint_state[10:]) * 180 / np.pi} and max: {max(joint_state[10:]) * 180 / np.pi}")
 
         state_interp_func = interp1d(joint_state_time, joint_state, kind='linear', fill_value='extrapolate')  # Linear interpolation
         state_interp = state_interp_func(joint_cmd_time)  # Resampled state signal
@@ -74,21 +153,37 @@ if __name__ == "__main__":
         delay_idx = np.argmax(correlation) - (len(joint_cmd_time) - 1)
         time_delay = delay_idx * (joint_cmd_time[1] - joint_cmd_time[0])  # Time between samples
 
-        print(f"{motor}: Estimated time delay: {time_delay} seconds")
+        # print(f"{motor}: Estimated time delay: {time_delay} seconds")
+
+        joint_cmd = remove_spikes(joint_cmd)
+
+        lags, cmd_crossings, state_crossings = get_lag(joint_cmd, joint_cmd_time, joint_state, joint_state_time, offsets[motor])
+        # print(f"{motor}: lag times {lags}")
+
+        print(f"{motor}: {ramp_rates[motor]} and {lags}")
+
+        # axs.flatten()[joint_idx].scatter(ramp_rates[motor], lags)
+        # axs.flatten()[joint_idx].set_ylabel(f'Joint {motor} lag (s)')
+
+        if motor == "RL_0":
+            print(joint_state)
         
         # Plot joint command
         axs.flatten()[joint_idx].plot(joint_cmd_time, joint_cmd, label=f'Command', color='b')
+        axs.flatten()[joint_idx].scatter(cmd_crossings, np.ones(len(cmd_crossings)) * offsets[motor], s=15)
         
         # Plot joint state
         axs.flatten()[joint_idx].plot(joint_state_time, joint_state, label=f'State', color='r', linestyle='--')
+        axs.flatten()[joint_idx].scatter(state_crossings, np.ones(len(state_crossings)) * offsets[motor], s=15)
 
         # plot joint limits
         axs.flatten()[joint_idx].axhline(JOINT_LIMITS[motor][0], color='black', linestyle="dotted")
         axs.flatten()[joint_idx].axhline(JOINT_LIMITS[motor][1], color='black', linestyle="dotted")
         
         axs.flatten()[joint_idx].set_ylabel(f'Joint {motor} angle (rad)')
-        axs.flatten()[joint_idx].legend()
-
-    axs.flatten()[-1].set_xlabel('Time')
+    
+    axs.flatten()[-1].legend()
+    axs.flatten()[-1].set_xlabel("Ramp Rate (rad/s)")
+    # axs.flatten()[-1].set_xlabel('Time')
     plt.tight_layout(rect=[0, 0, 1, 0.96])
     plt.show()
