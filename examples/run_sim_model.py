483ff42392128efe2ee77d997ed63d260defde52
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-29 22:14:55 -0800

    initial try at run_sim_model with proper mapping

---
+++ examples/run_sim_model.py
@@ -0,0 +1,225 @@
+import torch
+import sys
+import numpy as np
+import time
+
+import constants.unitree_legged_const as go2
+import examples.actor_critic as actor_critic
+import utils.client_utils as client_utils
+import utils.state_estimator as se
+
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_
+from unitree_sdk2py.utils.crc import CRC
+from unitree_sdk2py.utils.thread import RecurrentThread
+
+class CmdMode:
+    TO_POSITION = 0
+    POLICY = 1
+    DAMP = 2
+    NONE = 3
+
+class normalization:
+    class obs_scales:
+        lin_vel = 2.0
+        ang_vel = 0.25
+        dof_pos = 1.0
+        dof_vel = 0.05
+        height_measurements = 5.0
+    clip_observations = 100.
+    clip_actions = 100.
+
+class control:
+    # PD Drive parameters:
+    control_type = 'P'
+    stiffness = {'joint': 20.}  # [N*m/rad]
+    damping = {'joint': 0.5}     # [N*m*s/rad]
+    # action scale: target angle = actionScale * action + defaultAngle
+    action_scale = 0.25
+    # decimation: Number of control action updates @ sim DT per policy DT
+    decimation = 4
+
+class ModelRunner:
+    def __init__(self, publisher_frequency):
+
+        if len(sys.argv)>1:
+            ChannelFactoryInitialize(0, sys.argv[1])
+        else:
+            ChannelFactoryInitialize(0)
+
+        rsc = client_utils.get_robot_state_client()
+        client_utils.set_service(rsc, "sport_mode", False)
+
+        # Create a publisher to publish the data defined in UserData class
+        self.pub = ChannelPublisher("rt/lowcmd", LowCmd_)
+        self.pub.Init()
+
+        self.cmd = unitree_go_msg_dds__LowCmd_()
+        self.cmd.head[0]=0xFE
+        self.cmd.head[1]=0xEF
+        self.cmd.level_flag = 0xFF
+        self.cmd.gpio = 0
+        for i in range(20):
+            self.cmd.motor_cmd[i].mode = 0x01  # (PMSM) mode
+            self.cmd.motor_cmd[i].q = go2.PosStopF
+            self.cmd.motor_cmd[i].kp = 0
+            self.cmd.motor_cmd[i].dq = go2.VelStopF
+            self.cmd.motor_cmd[i].kd = 0
+            self.cmd.motor_cmd[i].tau = 0
+
+        self.model = None
+        self.Kp = control.stiffness["joint"]
+        self.Kd = control.damping["joint"]
+        self.publisher_frequency = publisher_frequency
+        # in real joint order
+        self.sit_pos = np.array([-0.1, 1.1, -2.0, -0.1, 1.1, -2.0, -0.1, 1.1, -2.6, -0.1, 1.1, -2.6])
+        # in sim order
+        self.sit_pos = self.sit_pos[self.state_estimator.joint_idxs]
+        self.default_dof_pos = self.sit_pos # used in unitree_rl_gym for initialization
+        self.state_estimator = se.StateEstimator()
+        self.cmd_mode = CmdMode.NONE
+        self.raw_actions = None
+        self.start_position = None
+        self.target_position = None
+        self.reached_position = False
+        self.position_percent = 0
+        self.duration_s = 1
+
+        self.joint_limits_list = list(go2.JOINT_LIMITS.values())
+
+    def start(self):
+        self.lowCmdWriteThreadPtr = RecurrentThread(
+            interval=1/self.publisher_frequency, target=self.LowCmdWrite, name="writebasiccmd"
+        )
+        self.lowCmdWriteThreadPtr.Start()
+
+    def load_pt_model(self, model_path):
+        print(f"Loading model: {model_path}")
+
+        model = actor_critic.ActorCritic(
+            num_actor_obs=48,
+            num_critic_obs=48,
+            num_actions=12,
+            actor_hidden_dims=[512, 256, 128],
+            critic_hidden_dims=[512, 256, 128],
+            activation='elu',
+            init_noise_std=1.0
+        )
+
+        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model_state_dict'])
+        model.eval()
+        self.model = model
+
+    def go_to_position(self, target_position):
+        self.reached_position = False
+        self.position_percent = 0
+        self.state_estimator.cmd_mode = CmdMode.TO_POSITION
+        self.start_position = self.state_estimator.get_dof_pos()
+        self.target_position = target_position
+        while not self.reached_position:
+            time.sleep(1/self.publisher_frequency)
+
+    def run_policy(self):
+        runner.state_estimator.run_mode = se.RunMode.NORMAL
+        runner.state_estimator.cmd_mode = CmdMode.POLICY
+        while True:
+            time.sleep(1/self.publisher_frequency / 4)
+            if self.state_estimator.run_mode == se.RunMode.DAMP:
+                runner.state_estimator.cmd_mode = CmdMode.DAMP
+            elif self.state_estimator.run_mode == se.RunMode.SIT:
+                self.go_to_position(self.sit_pos)
+                # force program to go into damp mode afterwards
+                self.state_estimator.run_mode = se.RunMode.DAMP
+                runner.state_estimator.cmd_mode = CmdMode.DAMP
+
+    def get_observations(self):
+        """
+        From unitree_rl_gym:
+            self.obs_buf = torch.cat((  self.base_ang_vel  * self.obs_scales.ang_vel,
+                                        self.projected_gravity,
+                                        (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
+                                        self.dof_vel * self.obs_scales.dof_vel,
+                                        self.actions
+                                        ),dim=-1)
+        """
+        obs = np.concatenate(
+            (
+                self.state_estimator.get_body_angular_vel() * normalization.obs_scales.ang_vel,
+                self.state_estimator.get_gravity_vector(),
+                (self.state_estimator.get_dof_pos() - self.default_dof_pos) * normalization.obs_scales.dof_pos,
+                self.state_estimator.get_dof_vel() * normalization.obs_scales.dof_vel,
+            )
+        )
+
+        if self.raw_actions is None:
+            # check ordering
+            self.raw_actions = (self.state_estimator.get_dof_pos() - self.default_dof_pos) / control.action_scale
+
+        obs = np.concatenate((obs, self.raw_actions))
+        obs = obs.astype(np.float32).reshape(1, -1)
+
+        obs = torch.clip(obs, -normalization.clip_observations, normalization.clip_observations)
+        return obs
+
+    def LowCmdWrite(self):
+        if self.state_estimator.run_mode == CmdMode.NONE:
+            return
+        elif self.state_estimator.run_mode == CmdMode.DAMP:
+            dof_pos = self.state_estimator.get_dof_pos()
+            for i in range(12):
+                self.cmd.motor_cmd[i].q = dof_pos[i]
+                self.cmd.motor_cmd[i].dq = 0.0
+                self.cmd.motor_cmd[i].kp = 40
+                self.cmd.motor_cmd[i].kd = 5
+                self.cmd.motor_cmd[i].tau = 0.0
+        elif self.state_estimator.run_mode == CmdMode.TO_POSITION:
+            self.position_percent += 1 / self.duration_s / self.publisher_frequency
+            self.position_percent = min(self.position_percent, 1)
+            for i in range(12):
+                self.cmd.motor_cmd[i].q = (1 - self.position_percent) * self.start_position[i] + self.position_percent * self.target_position[i]
+                self.cmd.motor_cmd[i].dq = 0
+                self.cmd.motor_cmd[i].kp = self.Kp
+                self.cmd.motor_cmd[i].kd = self.Kd
+                self.cmd.motor_cmd[i].tau = 0
+        elif self.state_estimator.run_mode == CmdMode.POLICY:
+            obs = self.get_observations()
+            try:
+                output_actions = self.model.actor(torch.from_numpy(obs))
+                output_actions = torch.clip(output_actions, -normalization.clip_actions, normalization.clip_actions)
+                self.raw_actions = output_actions[0].detach().numpy()
+            except Exception as e:
+                print(f"Inference failed. {e}")
+
+            self.update_cmd_from_raw_actions(output_actions)
+        else:
+            raise NotImplementedError(f"{self.state_estimator.run_mode} run mode not implemented!")
+
+        self.cmd.crc = self.crc.Crc(self.cmd)
+        self.pub.Write(self.cmd)
+
+    def update_cmd_from_raw_actions(self, output_actions):
+        position_targets = output_actions * control.action_scale + self.default_dof_pos
+        for i in range(12):
+            q = position_targets[self.state_estimator.joint_idxs[i]]
+            q = max(min(q, self.joint_limits_list[i][1]), self.joint_limits_list[i][0])
+            self.cmd.motor_cmd[i].q = q
+            self.cmd.motor_cmd[i].dq = 0
+            self.cmd.motor_cmd[i].kp = self.Kp
+            self.cmd.motor_cmd[i].kd = self.Kd
+            self.cmd.motor_cmd[i].tau = 0
+
+if __name__ == '__main__':
+
+    runner = ModelRunner(publisher_frequency=250)
+
+    model_path = "models/model_1500.pt"
+    runner.load_pt_model(model_path)
+
+    # start by hanging position from sit (TODO: persist command)
+    runner.go_to_position(runner.sit_pos)
+
+    # start model loop
+    runner.run_policy()
+
+    # use this to publish consistently: example/go2/low_level/go2_stand_example.py
--- utils/state_estimator.py
+++ utils/state_estimator.py
@@ -1,7 +1,12 @@
+"""
+State Estimator, modified from https://github.com/Teddy-Liao/walk-these-ways-go2/blob/main/go2_gym_deploy/utils/cheetah_state_estimator.py#L52
+"""
+
 import time as time
 import numpy as np
 import math
-from filterpy.kalman import KalmanFilter
+import constants.unitree_legged_const as go2
+from scipy.spatial.transform import Rotation as R
 
 from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
 from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowState_
@@ -9,81 +14,24 @@ from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_
 from unitree_sdk2py.idl.default import unitree_go_msg_dds__WirelessController_
 from unitree_sdk2py.idl.unitree_go.msg.dds_ import WirelessController_
 
-# https://github.com/Teddy-Liao/walk-these-ways-go2/blob/main/go2_gym_deploy/utils/cheetah_state_estimator.py#L52
-
-def get_rpy_from_quaternion(q):
-    w, x, y, z = q
-    r = np.arctan2(2 * (w * x + y * z), 1 - 2 * (x ** 2 + y ** 2))
-    p = np.arcsin(2 * (w * y - z * x))
-    y = np.arctan2(2 * (w * z + x * y), 1 - 2 * (y ** 2 + z ** 2))
-    return np.array([r, p, y])
-
-
-def get_rotation_matrix_from_rpy(rpy):
-    """
-    Get rotation matrix from the given quaternion.
-    Args:
-        q (np.array[float[4]]): quaternion [w,x,y,z]
-    Returns:
-        np.array[float[3,3]]: rotation matrix.
-    """
-    r, p, y = rpy
-    R_x = np.array([[1, 0, 0],
-                    [0, math.cos(r), -math.sin(r)],
-                    [0, math.sin(r), math.cos(r)]
-                    ])
-
-    R_y = np.array([[math.cos(p), 0, math.sin(p)],
-                    [0, 1, 0],
-                    [-math.sin(p), 0, math.cos(p)]
-                    ])
-
-    R_z = np.array([[math.cos(y), -math.sin(y), 0],
-                    [math.sin(y), math.cos(y), 0],
-                    [0, 0, 1]
-                    ])
-
-    rot = np.dot(R_z, np.dot(R_y, R_x))
-    return rot
+# will run normally until either sit or damp is called. damp supersedes sit
+class RunMode:
+    NORMAL = 0
+    SIT = 1
+    DAMP = 2
 
 class StateEstimator:
     def __init__(self):
         
-        # reverse legs
-        # self.joint_idxs = [3, 4, 5, 0, 1, 2, 9, 10, 11, 6, 7, 8]
-        # 0-FR, 1-FL, 2-RR, 3-RL
-        self.contact_idxs = [1, 0, 3, 2]
-        self.joint_idxs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        # mapping from real joints to simulation dof pos order
+        # actually goes both ways for this particular array
+        self.joint_idxs = [3, 4, 5, 0, 1, 2, 9, 10, 11, 6, 7, 8]
 
         self.joint_pos = np.zeros(12)
         self.joint_vel = np.zeros(12)
         self.tau_est = np.zeros(12)
-        self.world_lin_vel = np.zeros(3)
-        self.world_ang_vel = np.zeros(3)
-        self.euler = np.zeros(3)
-        self.R = np.eye(3)
-        self.buf_idx = 0
-
-        self.smoothing_length = 12
-        self.deuler_history = np.zeros((self.smoothing_length, 3))
-        self.dt_history = np.zeros((self.smoothing_length, 1))
-        self.euler_prev = np.zeros(3)
-        self.timuprev = time.time()
-
-        self.body_lin_vel = np.zeros(3)
         self.body_ang_vel = np.zeros(3)
-        self.smoothing_ratio = 0.2
-
-        self.contact_state = np.ones(4)
-
-        # Kalman Filter initialization
-        self.kf = KalmanFilter(dim_x=3, dim_z=3)
-        self.kf.x = np.zeros(3)  # initial velocity estimate (x, y, z)
-        self.kf.F = np.eye(3)    # state transition matrix
-        self.kf.H = np.eye(3)    # measurement function (direct observation)
-        self.kf.P *= 1.0         # initial uncertainty
-        self.kf.R = np.eye(3) * 0.1  # measurement noise
-        self.kf.Q = np.eye(3) * 0.1  # process noise
+        self.imu_quat = np.zeros(4)
 
         self.init_time = time.time()
         self.received_first_legdata = False
@@ -94,64 +42,34 @@ class StateEstimator:
         self.rc_command_subscription = ChannelSubscriber("rt/wirelesscontroller", WirelessController_)
         self.rc_command_subscription.Init(self._rc_command_cb, 10)
 
-        self.body_loc = np.array([0, 0, 0])
-        self.body_quat = np.array([0, 0, 0, 1])
-
-        self.stop_program = False
-
-    def get_body_linear_vel(self):
-        self.body_lin_vel = np.dot(self.R.T, self.world_lin_vel)
-        return self.body_lin_vel
-    
-    # CHATGPT
-    def get_body_linear_vel(self):
-        """Estimate body linear velocity using the Kalman Filter."""
-        # Rotation matrix to body frame
-        body_vel_estimate = np.dot(self.R.T, self.world_lin_vel)
-
-        # Apply Kalman Filter: Assume self.world_lin_vel is derived from accelerometer (prediction)
-        delta_time = time.time() - self.timuprev
-        self.kf.predict()
-        
-        # Update the filter if contacts are detected
-        observed_velocities = self.get_observed_velocities()
-        if len(observed_velocities) > 0:
-            mean_observed_velocity = np.mean(observed_velocities, axis=0)
-            self.kf.update(mean_observed_velocity)
-        else:
-            # Without contacts, use the predicted value with high uncertainty
-            self.kf.update(body_vel_estimate + np.random.randn(3) * 0.1)
-
-        # Update world velocity from Kalman Filter result
-        self.world_lin_vel = self.kf.x  # Kalman filter velocity estimate
-        self.body_lin_vel = np.dot(self.R.T, self.world_lin_vel)
-        return self.body_lin_vel
-
-    def get_observed_velocities(self):
-        """Calculate velocities based on foot contacts."""
-        observed_velocities = []
-        for i in range(4):
-            if self.contact_state[i]:
-                # Compute observed velocity for foot i and append
-                # (similar to C++ observed velocity calculation)
-                observed_velocity = np.zeros(3)  # Replace with actual calculation
-                observed_velocities.append(observed_velocity)
-        return observed_velocities
+        self.run_mode = RunMode.NORMAL
+        self.key_state = [
+            ["R1", 0],
+            ["L1", 0],
+            ["start", 0],
+            ["select", 0],
+            ["R2", 0],
+            ["L2", 0],
+            ["F1", 0],
+            ["F2", 0],
+            ["A", 0],
+            ["B", 0],
+            ["X", 0],
+            ["Y", 0],
+            ["up", 0],
+            ["right", 0],
+            ["down", 0],
+            ["left", 0],
+        ]
 
     def get_body_angular_vel(self):
-        self.body_ang_vel = self.smoothing_ratio * np.mean(self.deuler_history / self.dt_history, axis=0) + (
-                    1 - self.smoothing_ratio) * self.body_ang_vel
         return self.body_ang_vel
 
     def get_gravity_vector(self):
-        grav = np.dot(self.R.T, np.array([0, 0, -1]))
-        return grav
-
-    def get_contact_state(self):
-        return self.contact_state[self.contact_idxs]
-
-    def get_rpy(self):
-        return self.euler
+        rotation = R.from_quat(self.imu_quat)
+        gravity_vec_w = np.array([0.0, 0.0, -1.0])  # Gravity vector in world
+        gravity_proj = -1 * rotation.apply(gravity_vec_w)
+        return gravity_proj
 
     def get_dof_pos(self):
         return self.joint_pos[self.joint_idxs]
@@ -162,15 +80,6 @@ class StateEstimator:
     def get_tau_est(self):
         return self.tau_est[self.joint_idxs]
 
-    def get_yaw(self):
-        return self.euler[2]
-
-    def get_body_loc(self):
-        return np.array(self.body_loc)
-
-    def get_body_quat(self):
-        return np.array(self.body_quat)
-
     def _legdata_imu_cb(self, msg: LowState_):
         if not self.received_first_legdata:
             self.received_first_legdata = True
@@ -180,25 +89,29 @@ class StateEstimator:
         self.joint_vel = np.array(msg.motor_state.qd)
         self.tau_est = np.array(msg.motor_state.tau_est)
 
-        self.euler = np.array(msg.imu_state.rpy)
+        self.body_ang_vel = np.array([
+            msg.imu_state.gyroscope[0],
+            msg.imu_state.gyroscope[1],
+            msg.imu_state.gyroscope[2]
+        ])
 
-        self.R = get_rotation_matrix_from_rpy(self.euler)
-
-        # https://swanhub.co/AaronCheng/Instruction_Learning_Robot/blob/main/go1_gym_deploy/unitree_legged_sdk_bin/lcm_position.cpp
-        self.contact_state = 1.0 * (np.array(msg.foot_force) > 200)
-
-        self.deuler_history[self.buf_idx % self.smoothing_length, :] = msg.imu_state.rpy - self.euler_prev
-        self.dt_history[self.buf_idx % self.smoothing_length] = time.time() - self.timuprev
-
-        self.timuprev = time.time()
-
-        self.buf_idx += 1
-        self.euler_prev = np.array(msg.imu_state.rpy)
+        self.imu_quat = np.array([
+            msg.imu_state.quaternion[0],
+            msg.imu_state.quaternion[1],
+            msg.imu_state.quaternion[2],
+            msg.imu_state.quaternion[3]
+        ])
 
     def WirelessControllerHandler(self, msg: WirelessController_):
-        if msg.keys == 512:
-            print(f"Stop program!")
-            self.stop_program = True
+        for i in range(16):
+            self.key_state[i][1] = (msg.keys & (1 << i)) >> i
+
+        if self.key_state[9][1] == 1: # key B
+            print(f"Damping!")
+            self.run_mode = RunMode.DAMP
+        elif self.key_state[8][1] == 1: # key A
+            print(f"Sitting!")
+            self.run_mode = RunMode.SIT
 
     def close(self):
         self.low_state_subscription.Close()
