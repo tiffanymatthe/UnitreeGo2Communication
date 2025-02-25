9f6533a54dd689d424eaebc3eace56b2ce5a69dc
Tiffany Matthe <tiffanymatthe@gmail.com>
2025-02-25 08:50:03 -0800

    update plotting

--- scripts/plot_pkl.py
+++ scripts/plot_pkl.py
@@ -1,11 +1,34 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+JOINT_LIMITS = {
+    "FR_0": [-0.837758,0.837758],
+    "FR_1": [-1.5708,3.4907],
+    "FR_2": [-2.7227, -0.83776],
+    "FL_0": [-0.837758,0.837758],
+    "FL_1": [-1.5708,3.4907],
+    "FL_2": [-2.7227, -0.83776],
+    "RR_0": [-0.837758,0.837758],
+    "RR_1": [-0.5236,4.5379],
+    "RR_2": [-2.7227, -0.83776],
+    "RL_0": [-0.837758,0.837758],
+    "RL_1": [-0.5236,4.5379],
+    "RL_2": [-2.7227, -0.83776],
+}
 
 REAL_JOINT_LABELS = np.array(["FR_0","FR_1","FR_2","FL_0","FL_1","FL_2","RR_0","RR_1","RR_2","RL_0","RL_1","RL_2"])
 REAL_TO_SIM = [3, 4, 5, 0, 1, 2, 9, 10, 11, 6, 7, 8]
 
-pkl_file = "data/all_cmds_0_4_diagonal_works.pkl"
+pkl_file = "data/all_cmds_finished_0_4_diagonal_again.pkl"
+
+DOF_POS_OBS_SCALE = 1
+ACTION_SCALE = 0.25
+go2config_to_real = np.array([2,6,10,0,4,8,3,7,11,1,5,9])
+stand_pos_in_go2config = np.array([0.1,0.1,-0.1,-0.1,0.8,1,0.8,1,-1.5,-1.5,-1.5,-1.5])
+stand_pos_in_real = stand_pos_in_go2config[go2config_to_real]
+DEFAULT_POS_IN_SIM = stand_pos_in_real[REAL_TO_SIM]
+print(DEFAULT_POS_IN_SIM)
+print(REAL_JOINT_LABELS[REAL_TO_SIM])
 
 with (open(pkl_file, "rb")) as openfile:
     joint_commands = pickle.load(openfile)
@@ -19,6 +42,16 @@ with (open(pkl_file, "rb")) as openfile:
 
 # yaw pitch roll from gyroscope
 obs_times = [obs[0] for obs in joint_states]
+
+# Calculate average frequency from obs_times
+time_diffs = np.diff(obs_times)
+frequencies = 1 / time_diffs
+average_frequency = np.mean(frequencies)
+print(f"Average frequency: {average_frequency} Hz")
+# Calculate standard deviation of frequency
+std_dev_frequency = np.std(frequencies)
+print(f"Standard deviation of frequency: {std_dev_frequency} Hz")
+
 obs_modes = [obs[2] if len(obs) > 2 else 0 for obs in joint_states]
 angular_velocities = [obs[1][0][0:3] for obs in joint_states]
 grav_vectors = [obs[1][0][3:6] for obs in joint_states]
@@ -63,39 +96,38 @@ axs1 = axs1.flatten()
 fig2, axs2 = plt.subplots(4, 3, figsize=(12, 8))
 axs2 = axs2.flatten()
 
-# maybe this was changed in code? need to check
-default_pos_in_real = np.array([-0.1, 1.1, -2.0, -0.1, 1.1, -2.0, -0.1, 1.1, -2.6, -0.1, 1.1, -2.6])
-default_dof_pos_in_sim = default_pos_in_real[REAL_TO_SIM]
+cmd_times = [cmd[0] for cmd in joint_commands]
+cmd_modes = [cmd[1] for cmd in joint_commands]
+start_index_policy_cmd = cmd_modes.index(2)
+end_index_policy_cmd = cmd_modes.index(3)
 
 for i in range(12):
-    scaled_position = [x[i] + default_dof_pos_in_sim[i] for x in dof_positions][start_index_policy:end_index_policy]
-    scaled_action = [x[i] * 0.25 + default_dof_pos_in_sim[i] for x in policy_output_actions][start_index_policy:end_index_policy]
-    axs1[i].plot(obs_times[start_index_policy:end_index_policy], scaled_position, label="position (rad)") # use action_scale
-    axs1[i].plot(obs_times[start_index_policy:end_index_policy], scaled_action, label="action (rad)")
-    slope_of_action = np.gradient(scaled_action, obs_times[start_index_policy:end_index_policy])
-    axs2[i].plot(obs_times[start_index_policy:end_index_policy], slope_of_action * 180 / np.pi, label="slope of action (deg/s)")
-    # assume it runs at a frequency of 250 Hz (need to save time next time)
-    # Modify the data by changing each point relative to the previous point
-    # modified_positions = [policy_output_actions[0][i]]  # Start with the initial position
-    # max_angle_change = 30 * np.pi / 180 / 250
-    # for j in range(1, len(policy_output_actions)):
-    #     delta = policy_output_actions[j][i] - policy_output_actions[j-1][i]
-    #     if delta > max_angle_change:
-    #         delta = max_angle_change
-    #     elif delta < -max_angle_change:
-    #         delta = -max_angle_change
-    #     modified_positions.append(modified_positions[-1] + delta)
-    # axs1[i].plot(modified_positions, label="modified action")
+    scaled_position = [x[i] / DOF_POS_OBS_SCALE + DEFAULT_POS_IN_SIM[i] for x in dof_positions][start_index_policy:end_index_policy]
 
-    # real_idx = REAL_TO_SIM[i]
-    # # assume we stop program only after simulation
-    # # seems like joint commands do not change ... they keep the same as go to position
-    # # something wrong here...
-    # joint_cmd = [cmd[2][real_idx].q for cmd in joint_commands]
-    # axs1[i].plot(joint_cmd, label="cmd")
+    scaled_action = [x[i] * ACTION_SCALE + DEFAULT_POS_IN_SIM[i] for x in policy_output_actions][start_index_policy:end_index_policy]
+
+    trimmed_times = np.array(obs_times[start_index_policy:end_index_policy]) - obs_times[start_index_policy]
+    print(trimmed_times)
+
+    axs1[i].plot(trimmed_times, scaled_position, label="position (rad)") # use action_scale
+    axs1[i].plot(trimmed_times, scaled_action, label="action (rad)")
+    slope_of_action = np.gradient(scaled_action, trimmed_times)
+    axs2[i].plot(trimmed_times, slope_of_action * 180 / np.pi, label="slope of action (deg/s)")
+
+    real_idx = REAL_TO_SIM[i]
+    joint_cmd = [cmd[2][real_idx].q for cmd in joint_commands]
+    trimmed_cmd_times = np.array(cmd_times[start_index_policy_cmd:end_index_policy_cmd]) - obs_times[start_index_policy]
+    axs1[i].plot(trimmed_cmd_times, joint_cmd[start_index_policy_cmd:end_index_policy_cmd], label="cmd")
+
+    label = REAL_JOINT_LABELS[REAL_TO_SIM[i]]
+
+    axs1[i].axhline(JOINT_LIMITS[label][0], linestyle="--", color="black")
+    axs1[i].axhline(JOINT_LIMITS[label][1], linestyle="--", color="black")
     
-    axs1[i].set_title(REAL_JOINT_LABELS[REAL_TO_SIM[i]])
-    axs2[i].set_title(REAL_JOINT_LABELS[REAL_TO_SIM[i]])
+    axs1[i].set_title(label)
+    axs2[i].set_title(label)
+    if i == 11:
+        axs1[i].legend()
 
 plt.legend()
 
